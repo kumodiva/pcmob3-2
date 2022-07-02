25e76568c37906cd3b1351951385f938dfce6494
kumodiva <kumodiva@gmail.com>
2022-07-02 15:26:27 +0800

    Function to add new note

--- App.js
+++ App.js
@@ -1,30 +1,71 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Entypo } from "@expo/vector-icons";
+import * as SQLite from "expo-sqlite";
+import { FlatList } from "react-native-gesture-handler";
 
+const db = SQLite.openDatabase("notes.db");
 
 function NotesScreen({ navigation }) {
+  const [notes, setNotes] = useState([
+    { title: "Walk the cat", done: false, id: "0" },
+    { title: "Feed the elephant", done: false, id: "1" },
+  ]);
+ 
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => (
         <TouchableOpacity onPress={addNote}>
-          <Entypo name="new-message" size={24} color="black" />
+          <Entypo
+            name="new-message"
+            size={24}
+            color="black"
+            style={{ marginRight: 20 }}
+          />
         </TouchableOpacity>
       ),
     });
   });
-
-function addNote() {
-  console.log("Add Note");
-}
-
-return <View style={styles.container}></View>;
-}
-
-const Stack = createStackNavigator();
+ 
+  function addNote() {
+    let newNote = {
+      title: "Sample new note",
+      done: false,
+      id: notes.length.toString(),
+    };
+    setNotes([...notes, newNote]);
+  }
+ 
+  function renderItem({ item }) {
+    return (
+      <View
+        style={{
+          padding: 10,
+          paddingTop: 20,
+          paddingBottom: 20,
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+        }}
+      >
+        <Text style={{ textAlign: "left", fontSize: 16 }}>{item.title}</Text>
+      </View>
+    );
+  }
+ 
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={{ width: "100%" }}
+        data={notes}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+ }
+ const Stack = createStackNavigator();
 
 export default function App() {
  return (
@@ -38,7 +79,6 @@ export default function App() {
            headerTitleStyle: {
              fontWeight: "bold",
              fontSize: 30,
-             alignSelf: 'center',
            },
            headerStyle: {
              height: 120,
@@ -61,4 +101,3 @@ const styles = StyleSheet.create({
    justifyContent: "center",
  },
 });
-
